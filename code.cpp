eec63bcfee0aad7ed73f852cd74bfbddf80a0dc1
Krish4u27 <kkumar30220@gmail.com>
2024-10-21 12:08:25 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
- 
-int main (){
-    cout<<"hello world my name is krish";
-    
-return 0;
+
+int main()
+
+{
+    int num1, num2;
+    cout<<"Enter the value of num1:\n"; /* '<<' is called Insertion Operator */
+    cin>>num1; /* '>> is called Excertion Operator '*/
+
+    cout<<"Enter the value of num2:\n";
+    cin>>num2;
+
+    cout<<"The sum is "<< num1+num2;
+
+    return 0;
 }
